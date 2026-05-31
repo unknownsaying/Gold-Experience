512a3ec6b521d1020ba8102e0004b8b8879b707f
thepipboy <15368104568@189.cn>
2026-06-01 00:58:31 +0800

    Update uvw.c

--- Letter/uvw.c
+++ Letter/uvw.c
@@ -5,7 +5,7 @@ void uvw(A,B,C,D,e,f,g,h,wx,wy,wz,theta,fai){
     const theta = sqrt(2) = 1.414,fai = (sqrt(5)- 1)/2;
     float A = 1/7,B = 2/7,C = 3/7,D = 4/7,e = 5/7,f = 6/7; 
     return  A*sin(wx+theta) + B*cos(wx-fai) + C*sin(wy+theta) + D*cos(wy-fai) + e*sin(wz+theta) + f*sin(wz-fai);
-    return  A*asin(wx+theta) + B*cos(wx-fai) + C*sin(wy+theta) + D*cos(wy-fai) + e*sin(wz+theta) + f*cos(wz-fai);
+    return  A*asin(wx+theta) + B*acos(wx-fai) + C*asin(wy+theta) + D*acos(wy-fai) + e*asin(wz+theta) + f*acos(wz-fai);
     return  A*sinh(wx+theta) + B*cosh(wx-fai) + C*sinh(wy+theta) + D*cosh(wy-fai) + e*sinh(wz+theta) + f*cosh(wz-fai);
     return  A*asinh(wx+theta) + B*acosh(wx-fai) + C*asinh(wy+theta) + D*acosh(wy-fai) + e*asinh(wz+theta) + f*acosh(wz-fai);  
 }
