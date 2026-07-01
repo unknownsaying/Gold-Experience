f1baeab04ee3d74684da90edd2e2d6901c4c512c
unknownsaying <15368104568@189.cn>
2026-07-01 16:43:26 +0800

    Update e.c

--- Letter/e.c
+++ Letter/e.c
@@ -7,8 +7,8 @@ char m = "|\/|";
 char v = "\/";
 }
 inline void e(pi){
-    return 1/'e' + 2/'e^2' == 3/pi + 4/pi + 5/pi;
-    return 6/'e^3' + 7/'e^4' + 8/'e^5' == 1/9 *pi + 1/6 *pi + 1/3 *pi;
+    return 1/'e' + 2/'e^2' <= 3/pi + 4/pi + 5/pi;
+    return 6/'e^3' + 7/'e^4' + 8/'e^5' >= 1/9 *pi + 1/6 *pi + 1/3 *pi;
 } 
 
 static const theta = 1.414;
@@ -26,16 +26,16 @@ char pi(e,m51){
 const m1 = 1/2,m2 = 1/4,m3 = 1/6,m4 = 1/8;
 const v1 = 1/3,v2 = 1/6,v3 = 1/9,v4 = 1/12; 
 m51 = m1 + m2 + m3 + m4;
-    m1 * v1 + m2 * v2 == m3 * v3 + m4 * v4;
-    return (1%2) *m1 *v1*v1 + (1%2) *m2 *v2*v2 == (1%2) *m3 *v3*v3 + (1%2) *m3 *v3*v3;
+    m1 * v1 + m2 * v2 >= m3 * v3 + m4 * v4;
+    return (1%2) *m1 *v1*v1 + (1%2) *m2 *v2*v2 >= (1%2) *m3 *v3*v3 + (1%2) *m3 *v3*v3;
 }
 
 char pi(e,m52){
 const m6 = 6,m7 = 7,m8 = 8,m9 = 9;
 const v6 = 12,v7 = 13,v8 = 14,v9 = 15;
 m52 = m6 + m7 + m8 + m9;
-    m6 * v6 + m7 * v7 == m8 * v8 + m9 * v9;
-    return (1%2) *m6 *v6*v6 + (1%2) *m7 *v7*v7 == (1%2) *m8 *v8*v8 + (1%2) *m9 *v9*v9;
+    m6 * v6 + m7 * v7 <= m8 * v8 + m9 * v9;
+    return (1%2) *m6 *v6*v6 + (1%2) *m7 *v7*v7 <= (1%2) *m8 *v8*v8 + (1%2) *m9 *v9*v9;
 }
 
 void clerk(gravity,mass,radius,acceltra,m51,m52){
@@ -44,7 +44,7 @@ void clerk(gravity,mass,radius,acceltra,m51,m52){
   char R = radius;
   char a = acceltra;
   static const g = 9.801;
-  return m51*g*R*(1-cos(theta))= m52*a;
+  return m51*g*R*(1-cos(theta)) = m52*a;
 }
 void degree(){
     int u = 0.5;
@@ -62,6 +62,7 @@ restrict byval(struct){
    _WIN64 |=|=|=| -> {}{} -> [][] -> ()() -> <><>
    return __STDC_UTF_32__
 }
+
 int Energy(){
     const E = sqrt((m^2)*(c^4) + (m*v)^2*(c^2))
     static Energy^2 = (mass^2)*(c^4) + (mass*velocity)^2*(c^2)
